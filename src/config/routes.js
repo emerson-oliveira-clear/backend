926f135b74323fcbb9781ca2d4e46c73462d93f5
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-10-02 10:10:33 -0300

    UPDATE: created authentication

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,67 +1,24 @@
-
-
-
 const express = require('express')
-
+const auth = require('./auth')
 module.exports = function (server) {
-
-    const router = express.Router()
-    server.use('/api', router)
-
+    /*
+    * Rotas protegidas por Token JWT
+    */
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+    protectedApi.use(auth)
     const BillingCycle = require('../api/billingCycle/billingCycleService')
-
-    BillingCycle.register(router, '/billingCycles')
-
+    BillingCycle.register(protectedApi, '/billingCycles')
+    /*
+    * Rotas abertas
+    */
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+    const AuthService = require('../api/user/AuthService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
 }
-
-
-
-
-
-/*
-const express = require('express')
-
-const router = express.Router()
-
-const billingCycleSchema = require('../api/billingCycle/billingCycle')
-
-
-routes.get('/', async (req, res)=>{
-    let findAll = await billingCycleSchema.find({})
-    res.send(findAll)
-    
-})
-
-routes.post('/', async (req, res)=>{
-    let saveBill = await billingCycleSchema.create({
-        ...req.body
-    }) 
-
-    res.send(saveBill)
-})
-
-routes.put('/:id', async(req, res)=>{
-    let id = req.headers.id
-    let findEditItem = await billingCycleSchema.updateOne({_id:id}, {...req.body})
-    let findById = await billingCycleSchema.findById(id)
-
-    res.send(findById)
-    
-})
-
-routes.delete('/:id', async (req,res)=>{
-    let id = req.headers.id
-    let findDeleteItem = await billingCycleSchema.findByIdAndDelete(id)
-    res.send('Deletado!')
-})
-
-module.exports = routes
-
-*/
-
-
-
-
 
 
 
